1db6ac664d59e94736221eadc4d481aa9b66c9bf
AndreyChim <chimitcyrenov@mail.ru>
2025-11-26 16:54:56 +0800

    Change value to page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(5)
-  console.log(count)
-  console.log(setCount)
+  const [likes, setLikes] = useState(5)
+  
+  function increment() {
+    setLikes(likes +1)
+  }
+
+  function decrement() {
+    setLikes(likes -1)
+
+  }
+
   return (
     <div className="App">
-      {/* <h1>{likes}</h1>
+      <h1>{likes}</h1>
       <button onClick={increment}>Increment</button>
-      <button onClick={() => console.log (likes -=1)}>Deccrement</button> */}
+      <button onClick={decrement}>Decrement</button>
     </div>
   );
 }
