6b14f27d1f8762216feab0b0acabbcd95b0c7f80
AndreyChim <chimitcyrenov@mail.ru>
2025-11-27 20:04:13 +0800

    Create PostItem component & add styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
+import './styles/App.css'
+import PostItem from "./components/PostItem";
 
 function App() {
   const [value, setValue] = useState('text to input')
   
   return (
     <div className="App">
-      <ClassCounter/>
+      <PostItem/>
+      <PostItem/>
+      <PostItem/>
     </div>
   );
 }
