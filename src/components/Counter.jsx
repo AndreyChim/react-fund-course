2b614b7bf392fa922661290ab62a2ba69e241234
AndreyChim <chimitcyrenov@mail.ru>
2025-11-28 10:52:23 +0800

    Fix learning comments

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,18 +1,22 @@
 import React, { useState } from 'react'
 
 const Counter = function () {
-    const [count, setCount] =useState(0) // const count destructuring to array const [count, setCount]
+  // const count = useState(0)  // useState returns an array of two objects to console [0, f] f - The function is intended to change the state
+  // console.log(count)       
+
+  const [count, setCount] = useState(0) // const count destructuring to const [count, setCount]
+                                          
       console.log(count)                  // we see to console a count value
       console.log(setCount)               // we see to console a setCount function
 
-        function increment() {
+        function increment() { 
             setCount(count + 1) // we don't change count value directly count += 1 but by built-in function, e.g. setCount
-          }
+        }
         
-          function decrement() {
+        function decrement() {
             setCount(count - 1) 
         
-          }
+        }
         
         return (
         <div>
