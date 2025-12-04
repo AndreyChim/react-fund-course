1e2128af550301137cc14503ff80c3e37c74bcd1
AndreyChim <chimitcyrenov@mail.ru>
2025-12-04 21:26:59 +0800

    Add learning comments

--- src/components/UI/button/MyButton.jsx
+++ src/components/UI/button/MyButton.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import classes from './MyButton.module.css'
 
+
 const MyButton = ({children, ...props}) => {
   
-  console.log('children:', children)
+  console.log('Inserts whatever was between `<MyButton>` and `</MyButton>` tags; children:', children)
   console.log('props:', props)
   console.log('children, ...props:', {children, ...props})
   return (
+
     <button {...props} className={classes.myBtn}>
-      {children}
+        {children}
     </button>
   )
 }
 
 export default MyButton
+
+// Desctructure
+// const MyButton = ({children, ...props}) => { 
+  // <button {...props} className={classes.myBtn}>
+  //       {children}
+  // </button>
+
+// Without desctructure
+// const MyButton = (props) => {
+//   <button className={classes.myBtn}>
+//     {props.children}
+//   </button>
