71d35ee184bb38013363e7b25548cedc28322454
AndreyChim <chimitcyrenov@mail.ru>
2025-12-17 21:05:57 +0800

    Add useEffect and useRef hooks

--- src/components/PostFilter.jsx
+++ src/components/PostFilter.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
 import MyInput from './UI/input/MyInput'
 import MySelect from './UI/select/MySelect'
 
 const PostFilter = ({filter, setFilter}) => {
+  const renderCount = useRef(0);
+  
+  useEffect(() => {
+    renderCount.current += 1;
+    console.log(`📊 PostFilter Render #${renderCount.current}`);
+    console.log('   Current filter:', filter);
+    console.log('   Sort value:', filter.sort);
+    console.log('   Query value:', filter.query);
+    console.log('---');
+  }, [filter]);
+
   return (
     <div>
     <MyInput
