b167644e2726c405ac0788c379f46b2bb9a054d4
AndreyChim <chimitcyrenov@mail.ru>
2025-12-22 10:06:36 +0800

    Fix type current option

--- src/components/UI/select/MySelect.jsx
+++ src/components/UI/select/MySelect.jsx
@@ -2,30 +2,24 @@ import React from 'react'
 
 const MySelect = ({options, defaultValue, value, onChange}) => {
   // Log all parameters
-  console.log("All props:", { options, defaultValue });
+  console.log("All props MySelect:", { options, defaultValue, value, onChange });
   console.log("Options array:", options);
   console.log("Default value:", defaultValue);
 
   return (
     <select
         value={value}
-        onChange={event => onChange(event.target.value)}
+        onChange={event => {
+          console.log('Current option object:', event.target.value);
+          onChange(event.target.value)}
+        }
     >
         <option disabled value="">{defaultValue}</option>
-        {options.map(option => {
-            // Log each option's details
-            console.log('Current option object:', option);
-            console.log('Type of option:', typeof option);
-            console.log('Keys in option:', Object.keys(option));
-            console.log("Option value:", option.value);
-            console.log("Option name:", option.name);
-
-          return (
+        {options.map(option => (
             <option key={option.value} value={option.value}>
                 {option.name}
             </option>
-          );
-        })}
+        ))} 
     </select>
   )
 }
