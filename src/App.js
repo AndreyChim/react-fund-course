f3375cf4ec35645b581384d46fa49b442335fbd6
AndreyChim <chimitcyrenov@mail.ru>
2025-12-26 08:19:58 +0800

    Fix error cannot read properties of undefined localeCompare

--- src/App.js
+++ src/App.js
@@ -19,7 +19,15 @@ function App() {
   const [selectedSort, setSelectedSort] = useState('')
   const [searchQuery, setSearchQuery] = useState('')
 
-  const sortedPosts = [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+  function getSortedPosts() {
+      console.log('The sortedPosts function has completed')
+      if(selectedSort) {
+          return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+      }
+      return posts
+  }
+
+  const sortedPosts = getSortedPosts()
 
   const createPost = (newPost) =>
     setPosts([...posts, newPost])
