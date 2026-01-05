4e27cdeec4b87c70fa4139d0a303f359d185e0ec
AndreyChim <chimitcyrenov@mail.ru>
2026-01-05 23:10:38 +0800

    Fix animation

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,35 +1,42 @@
-import React, {useRef} from 'react'
+import React, { useRef } from 'react'
 import PostItem from './PostItem'
 import { TransitionGroup, CSSTransition } from 'react-transition-group'
 
-const PostList = ({posts, title, remove}) => {
+const PostList = ({ posts, title, remove }) => {
+  const nodeRefs = useRef({})
 
-  const nodeRef = useRef(null)
-  
+  posts.forEach(post => {
+    if (!nodeRefs.current[post.id]) {
+      nodeRefs.current[post.id] = React.createRef()
+    }
+  })
+
   if (!posts.length) {
-      return (
-          <h1 style={{textAlign: "center"}}>
-              No posts found!
-          </h1>
-      )
+    return (
+      <h1 style={{ textAlign: "center" }}>
+        No posts found!
+      </h1>
+    )
   }
-  
+
   return (
     <div>
-      <h1 style={{textAlign: 'center'}}>
-          {title}
+      <h1 style={{ textAlign: 'center' }}>
+        {title}
       </h1>
       <TransitionGroup>
-          {posts.map((post, index) =>
-              <CSSTransition
-                  key={post.id}
-                  nodeRef={nodeRef}
-                  timeout={500}
-                  classNames="post"
-              >
+        {posts.map((post, index) => (
+          <CSSTransition
+            key={post.id}
+            nodeRef={nodeRefs.current[post.id]}
+            timeout={500}
+            classNames="post"
+          >
+            <div ref={nodeRefs.current[post.id]}>
               <PostItem remove={remove} number={index + 1} post={post} />
-              </CSSTransition>
-       )}
+            </div>
+          </CSSTransition>
+        ))}
       </TransitionGroup>
     </div>
   )
