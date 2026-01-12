8cb5372117ccdb85c66c0e80249dc24e623f9729
AndreyChim <chimitcyrenov@mail.ru>
2026-01-12 16:30:08 +0800

    Create a custom hook

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import PostForm from "./components/PostForm";
 import MySelect from "./components/UI/select/MySelect";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
+import { usePosts } from "./hooks/usePosts";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -35,32 +36,7 @@ function App() {
       query: ''    // Search text
   });
   const [modal, setModal] = useState(false)
-
-  // filter.sort accesses the sort property from the filter object
-
-  const sortedPosts = useMemo(() => {
-    console.log('The sortedPosts function has completed')
-    if(filter.sort) {
-      console.log('filter:', filter) 
-      console.log("   filter an object:", typeof filter);
-      console.log('{...filter}:', {...filter})
-      console.log("   {...filter} an object:", typeof {...filter});
-      console.log("3. Are 'filter' and '{...filter}' the SAME object?");
-      // console.log("   Using '===':", filter === {...filter}); 
-      console.log("   Using Object.is():", Object.is(filter, {...filter})); 
-      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-  }
-    return posts;
-  }, [filter.sort, posts])
-
-
-  // const sortedAndSearchedPosts = useMemo(() => {
-  //     return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery.toLowerCase()))
-  // }, [searchQuery, posts])
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
-}, [filter.query, sortedPosts])
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
