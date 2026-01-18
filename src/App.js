c4ca8c335a8a1f9eea30b0e594fea4579c558230
AndreyChim <chimitcyrenov@mail.ru>
2026-01-18 13:18:47 +0800

    Add routing to posts

--- src/App.js
+++ src/App.js
@@ -13,12 +13,14 @@ import './styles/App.css';
 import { getPageCount } from "./utils/pages";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import About from "./pages/About";
+import Posts from "./pages/Posts";
 
 function App() {
   return (
     <BrowserRouter>
         <Routes>
             <Route path='/about' element={<About />} />
+            <Route path='/posts' element={<Posts />} />
         </Routes>
     </BrowserRouter>
   )
--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from "react";
-import PostService from "./API/PostService";
-import PostFilter from "./components/PostFilter";
-import PostForm from "./components/PostForm";
-import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import Loader from "./components/UI/Loader/Loader";
-import MyModal from "./components/UI/MyModal/MyModal";
-import Pagination from "./components/UI/pagination/Pagination";
-import { useFetching } from "./hooks/useFetching";
-import { usePosts } from "./hooks/usePosts";
-import { getPageCount } from "./utils/pages";
+import PostService from "../API/PostService";
+import PostFilter from "../components/PostFilter";
+import PostForm from "../components/PostForm";
+import PostList from "../components/PostList";
+import MyButton from "../components/UI/button/MyButton";
+import Loader from "../components/UI/Loader/Loader";
+import MyModal from "../components/UI/MyModal/MyModal";
+import Pagination from "../components/UI/pagination/Pagination";
+import { useFetching } from "../hooks/useFetching";
+import { usePosts } from "../hooks/usePosts";
+import { getPageCount } from "../utils/pages";
 
 function Posts() {
   const [posts, setPosts] = useState([])
