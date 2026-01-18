2d5068a19157e588e60c180d934b0cfce6500dbc
AndreyChim <chimitcyrenov@mail.ru>
2026-01-18 13:09:17 +0800

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,96 +1,27 @@
-import React, { useEffect, useMemo, useRef, useState } from "react";
-import Counter from "./components/Counter";
-import ClassCounter from "./components/ClassCounter";
-import './styles/App.css'
-import PostItem from "./components/PostItem";
+import React, { useEffect, useState } from "react";
+import PostService from "./API/PostService";
+import PostFilter from "./components/PostFilter";
+import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
-import PostForm from "./components/PostForm";
-import MySelect from "./components/UI/select/MySelect";
-import PostFilter from "./components/PostFilter";
-import MyModal from "./components/UI/MyModal/MyModal";
-import { usePosts } from "./hooks/usePosts";
-import axios from "axios";
-import PostService from "./API/PostService";
 import Loader from "./components/UI/Loader/Loader";
-import { useFetching } from "./hooks/useFetching";
-import { getPageCount, getPagesArray } from "./utils/pages";
+import MyModal from "./components/UI/MyModal/MyModal";
 import Pagination from "./components/UI/pagination/Pagination";
+import { useFetching } from "./hooks/useFetching";
+import { usePosts } from "./hooks/usePosts";
+import './styles/App.css';
+import { getPageCount } from "./utils/pages";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
 
 function App() {
-  const [posts, setPosts] = useState([])
-  const [filter, setFilter] = useState({
-      sort: '',    
-      query: ''
-  });
-  const [modal, setModal] = useState(false)
-  const [totalPages, setTotalPages] = useState(0)
-  const [limit, setLimit] = useState(10)
-  const [page, setPage] = useState(1)
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
-  
-   // const [fetchPosts, isPostsLoading, postError] = useFetching(async () => { 1 var
-  const [fetchPosts, isPostsLoading, postError] = useFetching(async (limit, page) => {
-    const response = await PostService.getAll(limit, page);
-    setPosts(response.data)
-    const totalCount = response.headers['x-total-count']
-    setTotalPages(getPageCount(totalCount, limit));
-  })
-
-  useEffect( () => {
-    fetchPosts(limit, page)
-  }, [])
-
-  // 1 var
-  // fetchPosts()
-  // }, [page])  1 var
-
-  const createPost = (newPost) => {
-    setPosts([...posts, newPost])
-    setModal(false)
-  }       
-
-  const removePost = (post) => {
-    setPosts(posts.filter(p => p.id !==post.id))
-  }
-
-  const changePage = (page) => {
-    setPage(page)
-    fetchPosts(limit, page)
-  }
-
-  //  1 var 
-  //   setPage(page)
-  // }
-
   return (
-    <div className="App">
-      <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
-          Create a post
-      </MyButton>
-      <MyModal visible={modal} setVisible={setModal}>
-          <PostForm create={createPost}/>
-      </MyModal>
-      <hr style={{margin: '15px 0'}}/>
-      <PostFilter 
-          filter={filter}
-          setFilter={setFilter}      
-      />
-      {postError &&
-          <h1>Error ${postError}</h1>
-      }
-      {isPostsLoading
-          ? <div style={{display: "flex", justifyContent: "center", marginTop: 50}}><Loader/></div>
-          : <PostList remove={removePost} posts={sortedAndSearchedPosts} title="List of posts 1"/>
-      }
-      <Pagination
-          page={page} 
-          changePage={changePage}
-          totalPages={totalPages}
-          />
-    </div>
-  );
+    <BrowserRouter>
+        <Routes>
+            <Route path='/about' element={<About />} />
+        </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App; 
