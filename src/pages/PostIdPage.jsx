badf075a59ff99d3e7e6cd2707c390e379a5db35
AndreyChim <chimitcyrenov@mail.ru>
2026-01-19 15:30:50 +0800

    Type comments

--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -7,13 +7,19 @@ import Loader from '../components/UI/Loader/Loader'
 const PostIdPage = () => {
   const params = useParams()
   const [post, setPost] = useState({})
+  const [comments, setComments] = useState([])
   const [fetchPostById, isLoading, error] = useFetching(async (id) => {
     const response = await PostService.getById(id)
     setPost(response.data)
   })
+  const [fetchComments, isComLoading, comError] = useFetching(async (id) => {
+    const response = await PostService.getCommentsByPostId(id)
+    setComments(response.data)
+  })
 
   useEffect(() => {
     fetchPostById(params.id)
+    fetchComments(params.id)
   }, [])
   
   return (
@@ -23,7 +29,21 @@ const PostIdPage = () => {
           ? <Loader/>  
           : <div>{post.id}. {post.title}</div>
       }
-      </div>
+      <h1>
+          Comments
+      </h1>
+      {isComLoading
+          ? <Loader/>
+          : <div>
+              {comments.map(comm =>
+                <div style={{marginTop: 15}}>
+                    <h5>{comm.email}</h5>
+                    <div>{comm.body}</div>
+                </div>
+              )}
+          </div>
+      }
+    </div>
   )
 }
 
