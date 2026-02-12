d8165a0ebae810eb0f3bf7e4f4197187ca7d8491
AndreyChim <chimitcyrenov@mail.ru>
2026-02-12 13:55:22 +0800

    Fix loading show all

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -75,7 +75,7 @@ function Posts() {
               {value:5, name: '5'},
               {value:10, name: '10'},
               {value:25, name: '25'},
-              {value:-1, name: 'Show all'},
+              {value:100, name: 'Show all'},
           ]}
       />
       {postError &&
